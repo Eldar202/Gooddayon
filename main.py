ad73df423f63ea8dd95a27174b36857c6d244a7b
Eldar202 <badamshinel@yandex.ru>
2021-11-08 18:16:44 +0200

    fatal

--- main.py
+++ main.py
@@ -1,34 +1,12 @@
-import math
-
-list = []
-for i in range(11):
-    m = math.factorial(6*i)
-    m1 = math.factorial(3*i)
-    m2 = math.factorial(i)
-    M = m/(m1 *(m2**3))
-    L = 545140134 * i + 13591409
-    X = 262537412640768000 **i
-    drob = (M * L)/X
-    list.append(drob)
-
-summa = 0
-for chislo in (list):
-    summa = summa + chislo
-    C = 426880 * math.sqrt(10005)
-    pi = C * (1/summa)
-print (pi)
+def medians(nums1, nums2):
+    nums3 = nums1 + nums2
+    nums3 = list(set(nums3))
+    x = len(nums3)
+    if x % 2 == 0:
+        print('объединенный список-', nums3, 'медиана -', (nums3[(x//2)-1]+ nums3[(x//2)])/2)
+    else:
+        print('объединенный список-', nums3, 'медиана -', nums3[x//2]
 
 
-list1 = []
-for i in range(11):
-    a = 4 / (8 * i + 1)
-    b = 2 / (8 * i + 4)
-    c = 1 / (8 * i + 5)
-    d = 1 / (8 * i + 6)
-    abcd = a - b - c - d
-    q = (16 ** (-i)) * abcd
-    list1.append(q)
-    summa1 = 0
-    for chislo1 in (list1):
-        summa1 = summa1 + chislo1
-print (summa1)
+
+
